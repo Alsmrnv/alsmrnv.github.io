caadc269ebd552d5c9566cdd09d16f052cda4fad
Aleksey Smirnov Sergeevich <144739387+Alsmrnv@users.noreply.github.com>
2024-05-16 18:11:11 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -27,3 +27,37 @@ document.addEventListener('DOMContentLoaded', () => {
         localStorage.setItem(TIMER_POPUP_SHOWN_KEY, 'true');
     });
 });
+
+
+(function () {
+    const second = 1000,
+          minute = second * 60,
+          hour = minute * 60,
+          day = hour * 24;
+
+    let today = new Date(),
+        dd = String(today.getDate()).padStart(2, "0"),
+        mm = String(today.getMonth() + 1).padStart(2, "0"),
+        yyyy = today.getFullYear(),
+        nextYear = yyyy + 3,
+        dayMonth = "05/30/",
+        graduation = dayMonth + yyyy;
+    
+    today = mm + "/" + dd + "/" + yyyy;
+    if (today > graduation) {
+      graduation = dayMonth + nextYear;
+    }
+    
+    const countDown = new Date(graduation).getTime(),
+        x = setInterval(function() {    
+  
+          const now = new Date().getTime(),
+                distance = countDown - now;
+  
+          document.getElementById("days").innerText = Math.floor(distance / (day)),
+            document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),
+            document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
+            document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
+  
+        }, 0)
+    }());
